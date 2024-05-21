1a9d7e6f8edc7afa218fe50ea8dfd3e1b627375b
Bruno Gonçalves <brunogcp28@gmail.com>
2024-05-21 08:19:10 -0400

    Aula 03 JS intermediário - Funções anonimas

---
+++ index.js
@@ -0,0 +1,12 @@
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(1,2)
+// console.log(resultadoDaSoma)
+
+
+let resultadoDaSoma = function(numero1, numero2){
+    return numero1 + numero2
+}
+console.log(resultadoDaSoma(10, 5))
